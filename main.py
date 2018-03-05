5d7e7b42c588b6e60bd2a1b90f877cea555b0e8a
Brian Faure <bfaure23@gmail.com>
2018-03-05 18:20:49 -0500

    added code for compiling location data

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 class Place:
-	def __init__(self,city=None,state=None,population=None):
+	def __init__(self,city=None,state=None,population=None,coordinates=None):
 		self.city=city
 		self.state=state
 		self.population=int(population)
+		self.coordinates=coordinates
 	def __str__(self):
 		return '%s, %s population: %d'%(self.city,self.state,self.population)
 	def __repr__(self):
@@ -21,7 +22,8 @@ def import_population_data(fname='data/sub-est2016_all.csv'):
 	f=open(fname,'r')
 	text=f.read()
 	lines=text.split('\n')
-	places=[]
+	place_dict={}
+	place_list=[]
 	for l in lines[1:]:
 		if len(l)>5:
 			items=l.split(',')
@@ -30,12 +32,100 @@ def import_population_data(fname='data/sub-est2016_all.csv'):
 				state=items[9]
 				pop=items[18] # 2016 population estimate
 				new_place=Place(city,state,pop)
-				places.append(new_place)
-	return places
+				place_list.append(new_place)
+				place_dict['%s, %s'%(city,state)]=new_place
+	return place_list,place_dict
 
-def import_boundary_data(fname='data/admin_level_8.geojson'):
+def import_boundary_data(place_dict,fname='data/admin_level_8.geojson'):
 	data=json.load(open(fname))
-	print data
+	num_matched=0
+	for i in range(len(data['features'])):
+		cur=data['features'][i]
+		if 'properties' in cur and 'geometry' in cur:
+			geo=cur['geometry']
+			if 'coordinates' in geo:
+				coords=geo['coordinates']
+			else: continue
+
+			props=cur['properties']
+			if 'name' in props and 'is_in:state' in props:
+				city=props['name']
+				state=props['is_in:state']
+			else: continue
+
+			city=city.replace('Township','township')
+
+			# here we have city, state, and coordinates for a certain location
+			# we need to match these to one of the items in the place_dict
+			try:
+				place_dict['%s, %s'%(city,state)].coordinates=coords 
+				num_matched+=1
+			except:
+				try:
+					place_dict['%s city, %s'%(city,state)].coordinates=coords 
+					num_matched+=1
+				except:
+					try:
+						place_dict['%s township, %s'%(city,state)].coordinates=coords 
+						num_matched+=1
+					except:
+						try:
+							place_dict['%s village, %s'%(city,state)].coordinates=coords 
+							num_matched+=1
+						except:
+							try:
+								place_dict['%s borough, %s'%(city,state)].coordinates=coords 
+								num_matched+=1
+							except:
+								try:
+									place_dict['%s town, %s'%(city,state)].coordinates=coords 
+									num_matched+=1
+								except:
+									continue
+									print "Couldn't match %s, %s"%(city,state)
+	return num_matched
+
+def save_compiled_data(place_list,fname='data/compiled.tsv'):
+	f=open(fname,'w')
+	for place in place_list:
+		if place.coordinates!=None:
+			f.write('%s\t%s\t%d\t%s\n'%(
+				place.city,
+				place.state,
+				place.population,
+				str(place.coordinates)))
+	f.close()
+
+def compile_data():
+	sys.stdout.write('\n'*2)
+	sys.stdout.write('='*25)
+	sys.stdout.write('\n')
+
+	sys.stdout.write('Importing population data...')
+	place_list,place_dict=import_population_data()
+	place_list.sort(key=operator.attrgetter('population'))
+	sys.stdout.write(' loaded %d locations!'%len(place_list))
+	
+	sys.stdout.write('\n')
+
+	if not os.path.exists('data/admin_level_8.geojson'):
+		sys.stdout.write('ERROR: Could not locate admin_level_8.geojson\n')
+		return
+	else:
+		sys.stdout.write('Boundary Data found!\n')
+
+	sys.stdout.write('Importing boundary data...')
+	num_matched=import_boundary_data(place_dict)
+	sys.stdout.write(' loaded %d boundaries!\n'%num_matched)
+
+	sys.stdout.write('Saving compiled data...')
+	save_compiled_data(place_list)
+	sys.stdout.write(' saved %d locations!\n'%num_matched)
+
+	sys.stdout.write('\n')
+	sys.stdout.write('='*25)
+	sys.stdout.write('\n'*2)
+
 
 class MainWindow(QWidget):
 	def __init__(self,places=None,parent=None):
@@ -78,37 +168,43 @@ class MainWindow(QWidget):
 		sys.stdout.write(' done!\n')		
 
 
+def load_compiled_data(fname='data/compiled.tsv'):
+	f=open(fname,'r')
+	lines=f.read().split('\n')
+	places=[]
+	for l in lines:
+		items=l.strip().split('\t')
+		if len(items)==4:
+			new_place={}
+			new_place['city']=items[0]
+			new_place['state']=items[1]
+			new_place['pop']=items[2]
+			new_place['coords']=items[3]
+			places.append(new_place)
+	return places
+
 
 def main():
-	sys.stdout.write('\n'*2)
-	sys.stdout.write('='*25)
-	sys.stdout.write('\n')
 
-	sys.stdout.write('Importing data...')
-	places=import_population_data()
-	places.sort(key=operator.attrgetter('population'))
-	sys.stdout.write(' loaded %d locations!'%len(places))
+	places=load_compiled_data()
+	print "Found %d cities."%len(places)
 
-	sys.stdout.write('\n')
+	for p in places:
+		print '%s, %s pop: %s'%(p['city'],p['state'],p['pop'])
 
 	if not os.path.exists('data/maps/usa_map.png'):
 		sys.stdout.write('ERROR: Could not locate usa_map.png\n')
 		sys.stdout.write('Download from https://drive.google.com/open?id=1zsNRWINdvrrSTeCG0q2IhZc-ak4krHaQ\n')
-		sys.stdout.write('and place at inside data/maps/\n')
+		sys.stdout.write('and place at \'data/maps/\'\n')
+		return
 	else:
 		sys.stdout.write('Map image found!\n')
-
-	boundary_data=import_boundary_data()
 
 	'''
 	pyqt_app=QApplication(sys.argv)
 	_=MainWindow()
 	sys.exit(pyqt_app.exec_())
 	'''
-
-	sys.stdout.write('\n')
-	sys.stdout.write('='*25)
-	sys.stdout.write('\n'*2)
 
 if __name__ == '__main__':
 	main()
