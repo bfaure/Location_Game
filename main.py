3d4d0958489f5f5dde465b02055e0d2cbfe871a1
Brian Faure <bfaure23@gmail.com>
2018-03-05 00:12:17 -0500

    added population data and can now parse for loc and pop

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+import operator
+
+class Place:
+	def __init__(self,city=None,state=None,population=None):
+		self.city=city
+		self.state=state
+		self.population=int(population)
+	def __str__(self):
+		return '%s, %s population: %d'%(self.city,self.state,self.population)
+	def __repr__(self):
+		return self.__str__()
+
+def import_population_data(fname='data/sub-est2016_all.csv'):
+	f=open(fname,'r')
+	text=f.read()
+	lines=text.split('\n')
+	places=[]
+	for l in lines[1:]:
+		if len(l)>5:
+			items=l.split(',')
+			if len(items)==19:			
+				city=items[8]
+				state=items[9]
+				pop=items[18] # 2016 population estimate
+				new_place=Place(city,state,pop)
+				places.append(new_place)
+	return places
+
+sys.stdout.write('\n'*2)
+sys.stdout.write('='*25)
+sys.stdout.write('\n')
+
+sys.stdout.write('Importing data...')
+places=import_population_data()
+places.sort(key=operator.attrgetter('population'))
+sys.stdout.write(' loaded %d locations!'%len(places))
+
+print places[-10:-1]
+
+sys.stdout.write('\n')
+sys.stdout.write('='*25)
+sys.stdout.write('\n'*2)
+
+
+
